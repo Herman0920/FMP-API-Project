f1a0d63f970f73756cb0297e287b557b12f0f3ab
Herman Leung <Herman@DerekdeMacBook-Air.local>
2026-02-18 23:16:41 +0800

    Configure Express server

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
